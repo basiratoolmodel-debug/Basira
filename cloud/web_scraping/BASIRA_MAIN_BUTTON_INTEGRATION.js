144785132e935263d1b317abc12e6dc6b41349c0
basiratoolmodel-debug <basira.toolmodel@gmail.com>
2026-05-09 00:40:01 +0300

    Delete cloud/web_scraping directory

--- cloud/web_scraping/BASIRA_MAIN_BUTTON_INTEGRATION.js
+++
@@ -1,9 +0,0 @@
-// Use this in the main Basira application Web Scraping button.
-// Replace the URL with your Cloudflare Pages URL or custom domain.
-
-function openBasiraWebScraping() {
-  window.location.href = "https://basira-web-scraping.pages.dev";
-}
-
-// Example React button:
-// <button onClick={openBasiraWebScraping}>Web Scraping</button>
