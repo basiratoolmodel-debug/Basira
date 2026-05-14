b6265bd7991f96e5169c112f4e0f86dfecb71e31
basiratoolmodel-debug <basira.toolmodel@gmail.com>
2026-05-14 20:01:37 +0300

    Delete Basira_local/basira-scraper directory

--- Basira_local/basira-scraper/basira-scraper/next.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Removed 'output: export' to enable API routes
-};
-
-module.exports = nextConfig;
--- Basira_local/basira-scraper/basira-scraper/pages/_app.js
+++
@@ -1,7 +0,0 @@
-import '../styles/globals.css';
-
-function BasiraScraperApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default BasiraScraperApp;
